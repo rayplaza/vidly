6ea1c4f68bc4cf113b583641c9b03c9ea69ed86d
Ray Plaza <munditoplaza@gmail.com>
2020-02-24 16:01:34 -0800

    rendered badge class dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
     };
+
+
+
     render() {
         return (
             <div>
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
