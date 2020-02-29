bde849af71232dc2855856fbd791561bf17726c2
Ray Plaza <munditoplaza@gmail.com>
2020-02-28 21:35:19 -0800

    passing event

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,9 +5,11 @@ class Counter extends Component {
         count: 0
     };
 
-    handleIncrement = () => {
+    handleIncrement = (product) => {
+        console.log(product);
         this.setState({ count: this.state.count + 1 })
-    }
+    };
+
 
 
 
@@ -15,7 +17,7 @@ class Counter extends Component {
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.handleIncrement(product)} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
