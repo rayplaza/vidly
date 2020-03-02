c25ce2df0ea7e8f82647a2efa807922bc93ab20f
Ray Plaza <munditoplaza@gmail.com>
2020-03-02 14:06:15 -0800

    delete movie complete

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -7,6 +7,12 @@ class Movies extends Component {
     };
 
 
+    handleDelete = (movie) => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({ movies });
+    }
+
+
     render() {
         return <table className="table">
             <thead>
@@ -15,15 +21,17 @@ class Movies extends Component {
                     <th>Genre</th>
                     <th>Stock</th>
                     <th>Rate</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
                 {this.state.movies.map(movie =>
-                    <tr>
+                    <tr key={movie._id}>
                         <td>{movie.title}</td>
                         <td>{movie.genre.name}</td>
                         <td>{movie.numberInStock}</td>
                         <td>{movie.dailyRentalRate}</td>
+                        <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                     </tr>)}
             </tbody>
         </table>
