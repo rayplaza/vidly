ec040dd20a3093346200f66896f3f82e23b639c9
Ray Plaza <munditoplaza@gmail.com>
2020-03-02 13:59:24 -0800

    created tables to movies component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,12 +6,27 @@ class Movies extends Component {
         movies: getMovies()
     };
 
-    handleDelete = move => {
-
-    };
 
     render() {
-
+        return <table className="table">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Genre</th>
+                    <th>Stock</th>
+                    <th>Rate</th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.movies.map(movie =>
+                    <tr>
+                        <td>{movie.title}</td>
+                        <td>{movie.genre.name}</td>
+                        <td>{movie.numberInStock}</td>
+                        <td>{movie.dailyRentalRate}</td>
+                    </tr>)}
+            </tbody>
+        </table>
     }
 }
 
